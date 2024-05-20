977c8cb62470514c17b354b9ef1ea7fda568a3e7
Gowda_Bro@143 <hackerbrogowda143@gmail.com>
2024-05-20 23:17:18 +0530

    Use Of htmlFor in react

--- Input.jsx
+++ Input.jsx
@@ -5,6 +5,7 @@ const FormComponent = () => {
 
     return (
         <div>
+            <h1>Use Of htmlFor in react</h1>
             {/* In summary, htmlFor is used in React to follow JavaScript conventions, avoid conflicts with reserved keywords, and provide better accessibility and user experience by linking labels with their corresponding form controls. */}
             {/* htmlFor IS USED IN REACT WHEN WE CLICKON THE LABEL IT HAS TO HEIGLATE THE AREA IT HAS TO PERFORM THE TASK */}
             <label htmlFor="username">Username:</label>
@@ -14,6 +15,7 @@ const FormComponent = () => {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
             />
+            
         </div>
     );
 };
